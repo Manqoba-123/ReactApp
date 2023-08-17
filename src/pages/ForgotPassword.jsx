fcdf3f20695f6798c566b1d702ef54873b00a4d7
Manqoba-123 <mlangenimanqoba@gmail.com>
2023-08-17 22:50:27 +0200

    updating forgor page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Google from "../components/Google"
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e){
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth,email);
+      toast.success("Email sent")
+    } catch (error) {
+      toast.error("Email does not exist");
+    }
   }
   return (
     <section>
@@ -19,7 +32,7 @@ export default function ForgotPassword() {
           alt='locked' className='w-full rounded' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
+          <form onSubmit={onSubmit} >
            <input type='email' id='email' value={email} 
            onChange={onChange}  placeholder='Email Address'
            className='w-full px-4 py-2 text-xl text-gray-700
