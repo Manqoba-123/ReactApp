44e7767094fc90e606a46394f400f42a944cae4c
Manqoba-123 <mlangenimanqoba@gmail.com>
2023-08-28 01:05:56 +0200

    create a slider component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {collection, getDoc, getDocs, limit, orderBy, query} from 'firebase/firestore'
+import {db} from '../firebase'
+import Slider from '../components/Slider'
+import Spinner from '../components/Spinner'
+import { useState } from 'react'
 
 export default function Home() {
+
   return (
-    <div>Home</div>
+    <div>
+      <Slider />
+    </div>
   )
 }
